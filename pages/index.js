022bcccc7a3d7765856a6e1efa5e68662b1b9aff
남궁혜진 <nkhj0404@gmail.com>
2022-10-29 13:52:17 +0900

    영화 fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
 import Seo from "../components/Seo";
+import { useState, useEffect } from "react";
+const API_KEY = "eaaf6e2593199686bcd4c28bcf688306";
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    console.log("useEffect");
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <Seo title="Home" />
-      <h1>Home</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
